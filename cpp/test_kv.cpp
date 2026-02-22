f6016aaad234a662dfb03b2564451658d56fe00d
Tran Van Tan Khoi <tranvantankhoi@gmail.com>
2026-02-22 11:50:23 +0000

    Added tests for the logging feature

--- cpp/test_kv.cpp
+++ cpp/test_kv.cpp
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
+#include <filesystem>
+#include <sstream>
 #include "database.h"
 
 // Helper to convert string literals to bytes for cleaner tests
@@ -13,8 +15,25 @@ bool operator==(const Entry& a, const Entry& b) {
     return a.key == b.key && a.val == b.val;
 }
 
-TEST(KVTest, SetUpdates) {
-    KV kv;
+
+const std::string test_db = ".test_db";
+
+// class KVTest : public ::testing::Test {
+//     protected:
+
+//     void SetUp() override {
+//         std::filesystem::remove(test_db);
+//     }
+
+//     void TearDown() override {
+//         std::filesystem::remove(test_db);
+//     }
+// };
+
+TEST(KVTest, BasicOperationsAndPersistence) {
+    KV kv(test_db);
+    error open_err = kv.Open();
+    std::cerr << open_err.message() << std::endl;
     ASSERT_FALSE(kv.Open());
 
     bytes key = to_bytes("conf");
@@ -52,7 +71,7 @@ TEST(KVTest, SetUpdates) {
     EXPECT_FALSE(d1);
     EXPECT_FALSE(d_err1);
 
-    // Test Del success
+    // 7. Test Del success
     auto [d2, d_err2] = kv.Del(key);
     EXPECT_TRUE(d2);
     EXPECT_FALSE(d_err2);
@@ -60,16 +79,38 @@ TEST(KVTest, SetUpdates) {
     // Verify final state remains correct
     auto [res, err4] = kv.Get(key);
     EXPECT_FALSE(res.has_value());
+
+    // Add another key before the last test
+    bytes new_key = to_bytes("new key");
+    bytes new_val = to_bytes("new val");
+    auto [upd4, s_err] = kv.Set(new_key, new_val);
+    ASSERT_TRUE(upd4);
+    ASSERT_FALSE(s_err);
+
+    // --- Persistence TEST ---
+    kv.Close();
+    ASSERT_FALSE(kv.Open());
+
+    auto [new_r1, new_err1] = kv.Get(key);
+    EXPECT_FALSE(new_r1.has_value());
+    EXPECT_FALSE(new_err1);
+
+    auto [new_r2, new_err2] = kv.Get(new_key);
+    ASSERT_TRUE(new_r2.has_value());
+    EXPECT_EQ(new_r2.value(), new_val);
+    EXPECT_FALSE(new_err2);
     
     kv.Close();
 }
 
 TEST(EntryTest, EncodeDecode) {
-    Entry ent{to_bytes("k1"), to_bytes("xxx")};
+    // 1. Test regular entry
+    Entry ent{to_bytes("k1"), to_bytes("xxx"), false};
 
     bytes expected_data = {
         std::byte{2}, std::byte{0}, std::byte{0}, std::byte{0}, // klen
         std::byte{3}, std::byte{0}, std::byte{0}, std::byte{0}, // vlen
+        std::byte{0},                                           // flag
         std::byte{'k'}, std::byte{'1'},                         // key
         std::byte{'x'}, std::byte{'x'}, std::byte{'x'}          // val
     };
@@ -86,4 +127,21 @@ TEST(EntryTest, EncodeDecode) {
 
     EXPECT_EQ(ent, decoded);
     EXPECT_FALSE(err);
+
+    // 2. Test deleted entry
+    Entry tomb{to_bytes("k2"), {}, true};
+    bytes expected_tomb = {
+        std::byte{2}, std::byte{0}, std::byte{0}, std::byte{0}, // klen
+        std::byte{0}, std::byte{0}, std::byte{0}, std::byte{0}, // vlen
+        std::byte{1},                                           // flag
+        std::byte{'k'}, std::byte{'2'}                          // key
+    };
+
+    EXPECT_EQ(tomb.Encode(), expected_tomb);
+
+    std::string s_tomb(reinterpret_cast<const char *>(expected_tomb.data()), expected_tomb.size());
+    std::stringstream ss_tomb(s_tomb);
+    Entry decoded_tomb;
+    ASSERT_FALSE(decoded_tomb.Decode(ss_tomb));
+    EXPECT_EQ(tomb, decoded_tomb);
 }
